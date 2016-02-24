bbbcbc9751023185cb24807ad5eea9b02185c269
yachen <kingchen0824@gmail.com>
2016-02-24 15:34:50 -0800

    1111

---
+++ src/main/java/com/suning/acss/app/App.java
@@ -0,0 +1,11 @@
+package com.suning.acss.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow acss 2.0!");
+
+	}
+
+}
